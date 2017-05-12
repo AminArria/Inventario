da51c20899678c158d682efd794cd95417e48680
Amin Arria <arria.amin@gmail.com>
2017-05-12 11:51:41 -0400

    Specs for section model

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Section, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:section)).to be_valid
+  end
+
+  it 'is invalid without an api_id' do
+    section = build(:section, api_id: nil)
+    section.valid?
+    expect(section.errors[:api_id]).to include("can't be blank")
+  end
+
+  it 'is invalid without a name' do
+    section = build(:section, name: nil)
+    section.valid?
+    expect(section.errors[:name]).to include("can't be blank")
+  end
 end
