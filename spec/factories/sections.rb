3dfbd3391b0f3ba9915ea4e76e4aefa23ca138e3
Amin Arria <arria.amin@gmail.com>
2017-05-12 12:01:25 -0400

    Section factory

--- spec/factories/sections.rb
+++ spec/factories/sections.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :section do
-    api_id 1
-    name "MyString"
-    description "MyText"
+    api_id { Faker::Number.between(1, 10) }
+    name { Faker::Pokemon.name }
+    description { Faker::Lorem.paragraph(2) }
   end
 end
