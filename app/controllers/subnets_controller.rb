01fe881b88cecf0e088500311756a1d565364b73
Amin Arria <arria.amin@gmail.com>
2017-05-16 11:56:21 -0400

    Specs for controllers and modifications for them to pass

--- app/controllers/subnets_controller.rb
+++ app/controllers/subnets_controller.rb
@@ -2,13 +2,11 @@ class SubnetsController < ApplicationController
   before_action :set_subnet, only: [:show, :edit, :update, :destroy]
 
   # GET /subnets
-  # GET /subnets.json
   def index
     @subnets = Subnet.all
   end
 
   # GET /subnets/1
-  # GET /subnets/1.json
   def show
   end
 
@@ -22,43 +20,29 @@ class SubnetsController < ApplicationController
   end
 
   # POST /subnets
-  # POST /subnets.json
   def create
     @subnet = Subnet.new(subnet_params)
-
-    respond_to do |format|
-      if @subnet.save
-        format.html { redirect_to @subnet, notice: 'Subnet was successfully created.' }
-        format.json { render :show, status: :created, location: @subnet }
-      else
-        format.html { render :new }
-        format.json { render json: @subnet.errors, status: :unprocessable_entity }
-      end
+    if @subnet.save
+      redirect_to @subnet, notice: 'Subnet was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /subnets/1
-  # PATCH/PUT /subnets/1.json
   def update
-    respond_to do |format|
-      if @subnet.update(subnet_params)
-        format.html { redirect_to @subnet, notice: 'Subnet was successfully updated.' }
-        format.json { render :show, status: :ok, location: @subnet }
-      else
-        format.html { render :edit }
-        format.json { render json: @subnet.errors, status: :unprocessable_entity }
-      end
+    if @subnet.update(subnet_params)
+      redirect_to @subnet, notice: 'Subnet was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /subnets/1
-  # DELETE /subnets/1.json
   def destroy
     @subnet.destroy
-    respond_to do |format|
-      format.html { redirect_to subnets_url, notice: 'Subnet was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to subnets_url, notice: 'Subnet was successfully destroyed.'
   end
 
   private
@@ -69,6 +53,6 @@ class SubnetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def subnet_params
-      params.require(:subnet).permit(:api_id, :base, :mask, :section, :description)
+      params.require(:subnet).permit(:api_id, :base, :mask, :section_id, :description)
     end
 end
