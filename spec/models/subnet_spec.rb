b8d43c13fb287f326326630403e5d3dd9ab49660
Amin Arria <arria.amin@gmail.com>
2017-05-16 10:34:15 -0400

    Specs for subnet model

--- spec/models/subnet_spec.rb
+++ spec/models/subnet_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Subnet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:subnet)).to be_valid
+  end
+
+  it 'is invalid without an api_id' do
+    subnet = build(:subnet, api_id: nil)
+    subnet.valid?
+    expect(subnet.errors[:api_id]).to include("can't be blank")
+  end
+
+  it 'is invalid without a base' do
+    subnet = build(:subnet, base: nil)
+    subnet.valid?
+    expect(subnet.errors[:base]).to include("can't be blank")
+  end
+
+  it 'is invalid without a mask' do
+    subnet = build(:subnet, mask: nil)
+    subnet.valid?
+    expect(subnet.errors[:mask]).to include("can't be blank")
+  end
+
+  it 'is invalid without a section' do
+    subnet = build(:subnet, section: nil)
+    subnet.valid?
+    expect(subnet.errors[:section]).to include("must exist")
+  end
 end
