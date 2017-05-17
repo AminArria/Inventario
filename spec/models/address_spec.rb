fbf300180e1fbc2a13346a4adce2e161986d4078
Amin Arria <arria.amin@gmail.com>
2017-05-17 10:13:30 -0400

    Address model specs

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:address)).to be_valid
+  end
+
+  it 'is invalid without an api_id' do
+    address = build(:address, api_id: nil)
+    address.valid?
+    expect(address.errors[:api_id]).to include("can't be blank")
+  end
+
+  it 'is invalid without an ip' do
+    address = build(:address, ip: nil)
+    address.valid?
+    expect(address.errors[:ip]).to include("can't be blank")
+  end
+
+  it 'is invalid without a subnet' do
+    address = build(:address, subnet: nil)
+    address.valid?
+    expect(address.errors[:subnet]).to include("must exist")
+  end
+  it 'is created by default with an inactive status' do
+    address = create(:address)
+    expect(address.active).to be false
+  end
 end
