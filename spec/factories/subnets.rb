79ef8f183e1564c5fb1dd2789d59a5e867664e6f
Amin Arria <arria.amin@gmail.com>
2017-05-19 12:06:16 -0400

    Factory for new specs

--- spec/factories/subnets.rb
+++ spec/factories/subnets.rb
@@ -5,5 +5,10 @@ FactoryGirl.define do
     mask { Faker::Number.between(0, 32) }
     section
     description { Faker::Lorem.paragraph(2) }
+
+    factory :subnet_hosts do
+      max_hosts { Faker::Number.between(10, 20) }
+      used_hosts { Faker::Number.between(1, 10) }
+    end
   end
 end
