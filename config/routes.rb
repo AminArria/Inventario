1d939d28acca2e0bcb07c2d0c6535f916a335612
Amin Arria <arria.amin@gmail.com>
2017-05-19 11:17:16 -0400

    Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#dashboard'
+
   resources :sections, except: [:create, :destroy, :new] do
     resources :subnets, shallow: true do
       resources :addresses
