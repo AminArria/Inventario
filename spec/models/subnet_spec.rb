b8981e320594819ccb1ae2839dcd1e9a3d813ba7
Amin Arria <arria.amin@gmail.com>
2017-05-19 11:55:16 -0400

    Specs for statistics methods

--- spec/models/subnet_spec.rb
+++ spec/models/subnet_spec.rb
@@ -28,4 +28,38 @@ RSpec.describe Subnet, type: :model do
     subnet.valid?
     expect(subnet.errors[:section]).to include("must exist")
   end
+
+  it 'is invalid if used_hosts exceed max_hosts' do
+    subnet = build(:subnet, used_hosts: 2, max_hosts: 1)
+    subnet.valid?
+    expect(subnet.errors[:used_hosts]).to include("must be less than")
+  end
+
+  it 'is invalid if used_hosts is negative' do
+    subnet = build(:subnet, used_hosts: -1)
+    subnet.valid?
+    expect(subnet.errors[:used_hosts]).to include("must be greater than or equal to 0")
+  end
+
+  it 'is invalid if max_hosts is negative' do
+    subnet = build(:subnet, max_hosts: -1)
+    subnet.valid?
+    expect(subnet.errors[:used_hosts]).to include("must be greater than or equal to 0")
+  end
+
+  it 'gives the number of free hosts' do
+    subnet = create(:subnet_hosts)
+    expect(subnet.free_hosts).to eq 1
+  end
+
+  it 'gives the percentage of used hosts' do
+    subnet = create(:subnet_hosts)
+    expect(subnet.used_percentage).to eq (subnet.max_hosts / subnet.used_hosts * 100)
+  end
+
+  it 'gives the percentage of free hosts' do
+    subnet = create(:subnet_hosts)
+    expect(subnet.used_percentage).to eq (subnet.max_hosts / subnet.free_hosts * 100)
+  end
+
 end
