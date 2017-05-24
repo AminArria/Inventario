e182713525d4f02056e9107ab5cbb3549f33b6ad
Amin Arria <arria.amin@gmail.com>
2017-05-24 00:15:24 -0400

    Changed division to float division and method for CIDR format

--- app/models/subnet.rb
+++ app/models/subnet.rb
@@ -8,17 +8,22 @@ class Subnet < ApplicationRecord
   validates :public, inclusion: {in: [true, false]}, allow_blank: true
 
   before_save :define_type
+  # before_create :set_hosts
 
   def free_hosts
     self.max_hosts - self.used_hosts
   end
 
   def used_percentage
-    self.used_hosts / self.max_hosts * 100
+    self.used_hosts.to_f / self.max_hosts * 100
   end
 
   def free_percentage
-    self.free_hosts / self.max_hosts * 100
+    self.free_hosts.to_f / self.max_hosts * 100
+  end
+
+  def cidr
+    "#{self.base}/#{self.mask}"
   end
 
   private
@@ -31,4 +36,9 @@ class Subnet < ApplicationRecord
       self.public = true
     end
   end
+
+  # def set_hosts
+  #   self.max_hosts = 2 ** (32-self.mask) - 2
+  #   self.used_hosts = 0
+  # end
 end
