e648f731be12808561d8700acb4f210ce3b25e4a
Amin Arria <arria.amin@gmail.com>
2017-05-25 12:50:17 -0400

    Refactored Section#public_addresses_count

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,19 +4,19 @@ class Section < ApplicationRecord
   validates :api_id, :name, presence: true
 
   def public_addresses_count
-    counts = {max: 0, used: 0, free: 0}
-    self.subnets.where(public: true).each do |subnet|
-      counts[:max] += subnet.max_hosts
-      counts[:used] += subnet.used_hosts
-      counts[:free] += subnet.free_hosts
-    end
+    counts = {
+      max:  self.subnets.where(public: true).sum("max_hosts"),
+      used: self.subnets.where(public: true).joins(:addresses).count,
+    }
 
     if counts[:max] == 0
+      counts[:free] == 0
       counts[:used_percent] = 100
       counts[:free_percent] = 0
     else
+      counts[:free] = counts[:max] - counts[:used]
       counts[:used_percent] = counts[:used].to_f / counts[:max].to_f * 100
-      counts[:free_percent] = counts[:free].to_f / counts[:max].to_f * 100
+      counts[:free_percent] = counts[:free].to_f / counts[:max].to_f * 100      
     end
 
     counts
