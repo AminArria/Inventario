7b2bc331001036528bc5b823fa0f82908752037f
Amin Arria <arria.amin@gmail.com>
2017-05-25 15:36:30 -0400

    Report for whole system

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,9 @@ class StaticPagesController < ApplicationController
   def dashboard
     @sections = Section.all
   end
+
+  def generate
+    render xlsx: 'generate'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#dashboard'
 
+  get 'generate', to: "static_pages#generate", as: 'generate'
+  
   resources :sections, except: [:create, :destroy, :new] do
     resources :subnets, shallow: true do
       resources :addresses
