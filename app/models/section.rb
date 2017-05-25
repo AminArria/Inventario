55821ec202aaa75ef96afd9203e67fd61c6c2489
Amin Arria <arria.amin@gmail.com>
2017-05-25 15:26:49 -0400

    Method for all public addresses count

--- app/models/section.rb
+++ app/models/section.rb
@@ -22,4 +22,23 @@ class Section < ApplicationRecord
     counts
   end
 
+  def self.all_public_addresses_count
+    counts = {
+      max:  Subnet.where(public: true).sum("max_hosts"),
+      used: Subnet.where(public: true).joins(:addresses).count,
+    }
+
+    if counts[:max] == 0
+      counts[:free] == 0
+      counts[:used_percent] = 100
+      counts[:free_percent] = 0
+    else
+      counts[:free] = counts[:max] - counts[:used]
+      counts[:used_percent] = counts[:used].to_f / counts[:max].to_f * 100
+      counts[:free_percent] = counts[:free].to_f / counts[:max].to_f * 100      
+    end
+
+    counts
+  end
+
 end
