364cbc5678dcc57d14e10cb75c507a165c498230
Amin Arria <arria.amin@gmail.com>
2017-05-26 10:37:26 -0400

    Name for report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,10 @@
 class ReportsController < ApplicationController
+  def general
+    render xlsx: 'general', filename: "Informe_Capacidades_#{xls_date_format(DateTime.now)}"
+  end
+
+  private
+    def xls_date_format(date)
+      date.strftime('%d_%m_%Y')
+    end
 end
