7922edf48d9a7148bfce21adc5e38dbdf796348c
Amin Arria <arria.amin@gmail.com>
2017-06-06 11:07:46 -0400

    Added navbar elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def navbar_activate(element)
+    case element
+    when :dashboard
+      ["static_pages"].include?(params["controller"]) ? "active" : ""
+    when :network
+      ["sections","subnets","addresses"].include?(params["controller"]) ? "active" : ""
+    else
+      ""
+    end
+  end
 end
