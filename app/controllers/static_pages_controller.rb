b6938730e1203cc4af32738344b1c26bcb9ff7e9
Amin Arria <arria.amin@gmail.com>
2017-08-09 17:50:49 -0400

    Dashboard for dedicated hosting (Vmware)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,7 @@ class StaticPagesController < ApplicationController
   def dashboard
     @sections_public = Section.addresses_count(public: true)
     @sections_private = Section.addresses_count(public: false)
+
+    @dedicated_hosting = DataCenter.global_instance_stats
   end
 end
--- app/models/data_center.rb
+++ app/models/data_center.rb
@@ -27,4 +27,35 @@ class DataCenter < ApplicationRecord
   def memory_free
     self.clusters.sum {|c| c.memory_free}
   end
+
+  def instances_total
+    cpu = (cpu_total / ENV["vmware_instance_cpu"].to_i).to_i
+    memory = (memory_total / ENV["vmware_instance_memory"].to_i).to_i
+    return (cpu <= memory ? cpu : memory)
+  end
+
+  def instances_used
+    cpu = (cpu_used / ENV["vmware_instance_cpu"].to_i).to_i
+    memory = (memory_used / ENV["vmware_instance_memory"].to_i).to_i
+    return (cpu <= memory ? cpu : memory)
+  end
+
+
+  def self.global_instance_stats
+    stats = {
+      instances_total: 0,
+      instances_used: 0
+    }
+
+    DataCenter.all.each do |dc|
+      stats[:instances_total] += dc.instances_total
+      stats[:instances_used] += dc.instances_used
+    end
+
+    stats[:instances_free] = stats[:instances_total] - stats[:instances_used]
+    stats[:used_percent] = stats[:instances_used] / stats[:instances_total].to_f * 100
+    stats[:free_percent] = stats[:instances_free] / stats[:instances_total].to_f * 100
+
+    stats
+  end
 end
