5438749b825db03883f01339de42d5f07aabe3d0
Amin Arria <arria.amin@gmail.com>
2017-09-12 15:11:47 -0400

    Added env varibles for disk

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,4 +1,4 @@
 Figaro.require_keys("phpipam_base_url", "phpipam_username", "phpipam_password",\
-    "vmware_size", "vmware_instance_cpu", "vmware_instance_memory", "vmware_user",\
-    "vmware_password"
+    "vmware_size", "vmware_instance_cpu", "vmware_instance_memory",\
+    "vmware_instance_disk", "vmware_user", "vmware_password"
   )
