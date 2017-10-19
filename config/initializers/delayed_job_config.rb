babeb506222fbceb3b1100985bd62dd60448b1ab
Amin Arria <arria.amin@gmail.com>
2017-10-19 11:14:01 -0400

    Configured Delayed::Job

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,2 +1,5 @@
 # Configuration for Delayed::Job
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
